29b133525f213cecfce12021e0607edaeeff94e0
TheHindd <148694815+TheHindd@users.noreply.github.com>
2025-10-15 11:58:04 +0300

    privateRoute not working

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,13 +5,13 @@ import Login from './pages/Login'
 import ResetPassword from './pages/ResetPassword'
 import { ToastContainer, toast } from 'react-toastify';
 import Dashboard from './pages/Dashboard'
-import adminDashboard from './pages/Admin/adminDashboard'
-import managerDashboard from './pages/Manager/managerDashboard'
-import ManageUsers from './pages/Admin/ManageUsers'
-import createTask from './pages/Manager/createTask'
-import manageProjects from './pages/Manager/manageProjects'
-import manageTasks from './pages/Manager/manageTasks'
-import Privateroute from './components/Privateroute'
+// import adminDashboard from './pages/Admin/adminDashboard'
+// import managerDashboard from './pages/Manager/managerDashboard'
+// import ManageUsers from './pages/Admin/ManageUsers'
+// import createTask from './pages/Manager/createTask'
+// import manageProjects from './pages/Manager/manageProjects'
+// import manageTasks from './pages/Manager/manageTasks'
+//import Privateroute from './components/Privateroute'
 
 const App = () => {
   return (
@@ -25,18 +25,18 @@ const App = () => {
         <Route path='/Dashboard' element={<Dashboard/>} />
 
         {/* Admin Routes */}
-        <Route element={<Privateroute allowedRoles={"admin"}/>}>
+        {/* <Route element={<Privateroute allowedRoles={"admin"}/>}>
         <Route path='/admin/adminDashboard' element={<adminDashboard/>} />
         <Route path='/admin/ManageUsers' element={<ManageUsers/>} />
-        </Route>
+        </Route> */}
 
          {/* Manager Routes */}
-        <Route element={<Privateroute allowedRoles={"manager"}/>}>
+        {/* <Route element={<Privateroute allowedRoles={"manager"}/>}>
         <Route path='/manager/managerDashboard' element={<managerDashboard/>} />
         <Route path='/manager/createTask' element={<createTask/>} />
         <Route path='/manager/manageProjects' element={<manageProjects/>} />
         <Route path='/manager/manageTasks' element={<manageTasks/>} />
-        </Route>
+        </Route> */}
       </Routes>
     </div>
   )
